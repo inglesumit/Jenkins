f5fa902164a019babe9bdd47d4aa808957af1ab0
unknown <inglesumit2612@gmail.com>
2019-05-08 12:30:20 +0530

    \first java program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package com.scp.firstjenkins;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to jenkins integration with git and eclipse");
+	}
+}
